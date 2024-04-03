0121d4a9efcc9be6950bc868e35449e3334febf4
SuperMustard <mr.jiahanxin@gmail.com>
2024-04-03 19:33:12 -0400

    add checkout form layout

---
+++ frontEnd/angular-ecommerce/src/app/components/checkout/checkout.component.ts
@@ -0,0 +1,71 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-checkout',
+  templateUrl: './checkout.component.html',
+  styleUrl: './checkout.component.css'
+})
+export class CheckoutComponent {
+  checkoutFormGroup!: FormGroup;
+
+  totalPrice: number = 0;
+  totalQuantity: number = 0;
+  
+  constructor(private formBuilder: FormBuilder) { }
+
+  ngOnInit(): void {
+    
+    this.checkoutFormGroup = this.formBuilder.group({
+      customer: this.formBuilder.group({
+        firstName: [''],
+        lastName: [''],
+        email: ['']
+      }),
+      shippingAddress: this.formBuilder.group({
+        street: [''],
+        city: [''],
+        state: [''],
+        country: [''],
+        zipCode: ['']
+      }),
+      billingAddress: this.formBuilder.group({
+        street: [''],
+        city: [''],
+        state: [''],
+        country: [''],
+        zipCode: ['']
+      }),
+      creditCard: this.formBuilder.group({
+        cardType: [''],
+        nameOnCard: [''],
+        cardNumber: [''],
+        securityCode: [''],
+        expirationMonth: [''],
+        expirationYear: ['']
+      })
+    });
+  }
+
+  copyShippingAddressToBillingAddress(event: any) {
+
+    if (event.target.checked) {
+      //@ts-ignore: Object is possibly 'null'
+      this.checkoutFormGroup.controls.billingAddress
+      //@ts-ignore: Object is possibly 'null'
+            .setValue(this.checkoutFormGroup.controls.shippingAddress.value);
+    }
+    else {
+      //@ts-ignore: Object is possibly 'null'
+      this.checkoutFormGroup.controls.billingAddress.reset();
+    }
+    
+  }
+
+  onSubmit() {
+    if (this.checkoutFormGroup) {
+      //@ts-ignore: Object is possibly 'null'
+      console.log(this.checkoutFormGroup.get('customer').value);
+    }
+  }
+}
